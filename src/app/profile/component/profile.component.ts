dbf3deba7e4c1ff181a743e5d4f9bc3adb17b3b7
Dzmitry Bahutski <kolinval2409@gmail.com>
2025-08-25 16:17:11 +0300

    refactor: add memory leak control to profile

--- src/app/profile/component/profile.component.ts
+++ src/app/profile/component/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -15,12 +16,13 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnDestroy{
+  private routerSubscription: Subscription;
   activeLink:string = '';
 
   constructor(private router: Router){
     this.router.navigate(['profile/profile-details']);
-    this.router.events.subscribe(() => {
+    this.routerSubscription = this.router.events.subscribe(() => {
       this.activeLink = this.router.url;
     });
   }
@@ -28,5 +30,10 @@ export class ProfileComponent {
   logout(){
     localStorage.removeItem('token');
     location.reload();
+  }
+
+  ngOnDestroy(): void {
+    if(this.routerSubscription)
+      this.routerSubscription.unsubscribe();
   }
 }
